e7b1770ec4efa875d99a632ac8ab3a3efcbbdece
Gabriel Sarmento <gabrielfs.bot@gmail.com>
2020-11-02 19:12:57 -0300

    adding user password encrypt

--- shifu/handlers/user.py
+++ shifu/handlers/user.py
@@ -2,12 +2,14 @@ from marshmallow import ValidationError
 from tornado.web import RequestHandler
 
 from shifu.handlers.serializers import UserSchema
+from shifu.helpers.user import encrypt_password
 
 
 class UserHandler(RequestHandler):  # noqa
     def post(self):
         try:
             user = UserSchema().loads(self.request.body)
+            user = encrypt_password(user)
             self.set_status(201)
             self.write(user)
         except ValidationError as e:
